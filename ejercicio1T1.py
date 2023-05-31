7f0f73130806971b8d582626656c448f6503ad69
ximenaAp002 <A01639678@tec.mx>
2023-05-30 19:18:42 -0600

    Final details

--- ejercicio1T1.py
+++ ejercicio1T1.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+import numpy as np
+from sklearn import datasets
+import itertools
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import KFold
 
 # unused but required import for doing 3d projections with matplotlib < 3.2
 import mpl_toolkits.mplot3d  # noqa: F401
-
-from sklearn import datasets
-from sklearn.decomposition import PCA
 
 # import some data to play with
 iris = datasets.load_iris()
@@ -36,7 +39,6 @@ plt.yticks(())
 
 plt.show()
 
-iris = datasets.load_iris()
 X = iris.data[:, 2:]  # we only take the last two features.
 y = iris.target
 print(y)
@@ -63,9 +65,7 @@ plt.yticks(())
 
 plt.show()
 
-#Using SVC for petals
-
-# Dividir los datos en conjunto de entrenamiento y prueba
+# Conjunto de entrenamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Crear y entrenar el clasificador SVM lineal
@@ -92,3 +92,71 @@ y_new_pred = clf.predict(X_new)
 print("Predicciones en los datos nuevos:")
 for i, prediction in enumerate(y_new_pred):
     print("Dato {}: Predicción {}".format(i+1, prediction))
+
+# Calcular la matriz de confusión en los datos de prueba
+cm = confusion_matrix(y_test, y_pred)
+
+# Función para graficar la matriz de confusión
+def plot_confusion_matrix(cm, classes, normalize=False, title='Matriz de Confusión', cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('Etiqueta verdadera')
+    plt.xlabel('Etiqueta predicha')
+
+# Definir las etiquetas de las clases
+class_labels = ['Setosa', 'Versicolor', 'Virginica']
+
+# Graficar la matriz de confusión
+plot_confusion_matrix(cm, classes=class_labels, normalize=True)
+plt.show()
+
+# Realizar k-fold cross validation (k = 10)
+k = 10
+cv = KFold(n_splits=k, shuffle=True, random_state=42)
+
+# Realizar k-fold cross validation y obtener las métricas de evaluación
+accuracies = cross_val_score(clf, X, y, cv=cv, scoring='accuracy')
+precisions = cross_val_score(clf, X, y, cv=cv, scoring='precision_macro')
+recalls = cross_val_score(clf, X, y, cv=cv, scoring='recall_macro')
+
+# Calcular la media y desviación estándar de las métricas de evaluación
+mean_accuracy = accuracies.mean()
+std_accuracy = accuracies.std()
+mean_precision = precisions.mean()
+std_precision = precisions.std()
+mean_recall = recalls.mean()
+std_recall = recalls.std()
+
+# Imprimir los resultados
+print("Exactitud promedio: {:.3f} (+/- {:.3f})".format(mean_accuracy, std_accuracy))
+print("Precisión promedio: {:.3f} (+/- {:.3f})".format(mean_precision, std_precision))
+print("Recall promedio: {:.3f} (+/- {:.3f})".format(mean_recall, std_recall))
+
+# Calcular y mostrar la matriz de confusión promedio
+confusion_matrices = []
+for train_index, test_index in cv.split(X):
+    clf.fit(X[train_index], y[train_index])
+    y_pred = clf.predict(X[test_index])
+    cm = confusion_matrix(y[test_index], y_pred)
+    confusion_matrices.append(cm)
+
+average_cm = np.mean(confusion_matrices, axis=0)
+class_labels = ['Setosa', 'Versicolor', 'Virginica']
+plot_confusion_matrix(average_cm, classes=class_labels, normalize=True)
+plt.show()
--- ejercicio4.py
+++ ejercicio4.py
@@ -1,23 +1,129 @@
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.model_selection import cross_val_score
 import numpy as np
-from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from sklearn import datasets
+from sklearn import svm
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_validate
+
+####################################################################################################################################
 
 # Cargar el archivo de texto y procesar los datos
 file_path = "c:/Users/Admin/Desktop/Semestre 6/deep/ajustesModeloClasificacion/misterious_data_2.txt"
-data = np.genfromtxt(file_path, delimiter='\t')
-X = data[:, :-1]
-y = data[:, -1]
+data = np.loadtxt(file_path)
+x = data[:, 1:]
+y = data[:, 0]
 
-# Crear un regresor k-NN
-knn_regressor = KNeighborsRegressor(n_neighbors=5)
+####################################################################################################################################
 
-# Realizar la validación cruzada con 5 folds en los datos seleccionados
-scores = cross_val_score(knn_regressor, X, y, cv=10)
+# Obtener informacion de los datos misterios
+print("-------------------------------------------")
+print('Features:', len(x[0]))
+print('Classes:', len(set(y)))
+print("-------------------------------------------")
 
-# Imprimir los resultados de cada fold en los datos seleccionados
-print("Resultados de cada fold en los datos seleccionados:", scores)
+####################################################################################################################################
+#Parte 1
+#Calcular la exactitud de kNN para k = 1 a 40
 
-# Calcular la puntuación media en los datos seleccionados
-mean_score = scores.mean()
-print("Puntuación media en los datos seleccionados:", mean_score)
+
+best_k = None
+best_accuracy = 0.0
+
+for k in range(1, 41):
+
+    # Crear un clasificador K-NN
+    knn_Classifier = KNeighborsClassifier(n_neighbors=k)
+
+    # Realizar la validación cruzada con 5 folds y calcular la exactitud
+    scoresKNN = cross_validate(knn_Classifier, x, y, cv=5, scoring='accuracy')
+
+    # Calcular la exactitud media del modelo k-NN
+    average_accuracy = np.mean(scoresKNN['test_score'])
+
+    #Imprimir la exactitud media 
+    print("Exactitud media en los datos seleccionados para k =", k, ": ",average_accuracy)
+
+    # Actualizar el valor más alto de la media
+    if average_accuracy > best_accuracy:
+        best_accuracy = average_accuracy
+        best_k = k
+
+print("-------------------------------------------")
+print("El valor más alto de media es:", best_accuracy)
+print("Se obtuvo con k =", best_k)
+print("-------------------------------------------")
+print(" ")
+
+####################################################################################################################################
+
+best_c = None
+best_average = 0.0
+
+c = 0.000001
+while c < 0.0001:
+    
+    # Crear un clasificador SVM lineal
+    svm_classifierLinear = svm.SVC(kernel="linear", C = c) 
+
+    # Realizar la validación cruzada con 5 folds y calcular la exactitud
+    scoresLin = cross_validate(svm_classifierLinear, x, y, cv=5, scoring=('accuracy', 'recall_micro'))
+    
+    # Calcular la exactitud media del modelo SVM lineal
+    average = np.mean(scoresLin['test_accuracy'])
+
+    #Imprimir la exactitud media 
+    print("Parametro en c =", c,": ", average)
+
+    # Actualizar el valor más alto de la media
+    if average > best_average:
+        best_average = average
+        best_c = c
+
+    c += 0.000001
+
+print(" ")
+print("-------------------------------------------")
+print("El valor más alto de media es:", best_average)
+print("Se obtuvo con C =", best_c)
+print("-------------------------------------------")
+print(" ") 
+
+####################################################################################################################################
+
+best_cR = None
+best_avgRBF = 0.0
+
+g = 0.000001
+while g <= 0.0002:
+
+    # Crear un clasificador SVM rbf
+    svm_classifierRBF  = svm.SVC(kernel="rbf", gamma= g )
+
+    # Realizar la validación cruzada con 5 folds y calcular la exactitud
+    scoresRBF = cross_validate(svm_classifierRBF , x, y, cv=5, scoring=('accuracy', 'recall_micro'))
+
+    # Calcular la exactitud media del modelo SVM lineal
+    averageR = np.mean(scoresRBF['test_accuracy'])
+    
+    #Imprimir la exactitud media 
+    print("Parametro en c =", g,": ", averageR)
+
+    # Actualizar el valor más alto de la media
+    if averageR > best_avgRBF:
+        best_avgRBF = averageR
+        best_cR = g
+
+    g += 0.000001
+
+print(" ")
+print("-------------------------------------------")
+print("El valor más alto de media es:", best_cR)
+print("Se obtuvo con C =", best_avgRBF)
+print("-------------------------------------------")
+print(" ")
+    
+
+
